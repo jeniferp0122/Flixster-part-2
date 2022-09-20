4a119e0e79f9b3bca034c180ca8ca93694731e35
jeniferp0122 <97690362+jeniferp0122@users.noreply.github.com>
2022-09-19 23:54:57 -0400

    flixster part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Jenifer  Pantoja on 9/19/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+  
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
